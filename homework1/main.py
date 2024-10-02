90993321f54e86595ebb0b5db12133efc4850f44
BYCODDE <anaraliev217@gmail.com>
2024-10-02 12:19:44 +0400

    Completed the assignment

--- homework1/main.py
+++ homework1/main.py
@@ -1,5 +1,33 @@
 def main() -> None:
     # Write your solution here
+
+
+    print('Hello World!')
+
+    print(5 + 7)
+
+    print(37 - 8)
+
+    print(-90 * 4)
+
+    print(100 / 3)
+
+    print(0.2 + 0.1)
+
+    print(7.068 * 4.3)
+
+    print(8 ** 2)
+
+    print(9 ** 3)
+
+    print(5 / 3)
+
+    print(5 // 3)
+
+    print(99 % 7)
+
+
+
     pass
 
 
